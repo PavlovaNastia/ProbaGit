9648e4104ceca418fcf1c9847a80745e3d9f7600
PavlovaNastia <78953875+PavlovaNastia@users.noreply.github.com>
2021-09-16 18:25:48 +0300

    Second try

--- src/probagit/ProbaGit.java
+++ src/probagit/ProbaGit.java
@@ -39,7 +39,12 @@ public class ProbaGit {
         System.out.println("В заданном Вами числе сотен " + hundreds);
         System.out.println("Сумма всех цифр равна " + summ );
         
-
+       System.out.println("Введите значение температуры в градусах цельсия:");
+        Scanner temp = new Scanner(System.in); //объявили Scanner temp
+        double cels = temp.nextDouble(); //считали значение double cels
+        double far = cels * 9 / 5 + 32; //высчитали значение double far
+        
+        System.out.println("Если температура в градусах Цельсия равна: " + cels + ", то в градусах Фаренгейта это будет: " + far );
         
     }
     
