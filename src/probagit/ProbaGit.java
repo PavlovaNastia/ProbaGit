80b749011f5e8305e36699999d6ad8a0c9ee122c
PavlovaNastia <78953875+PavlovaNastia@users.noreply.github.com>
2021-09-16 18:05:47 +0300

    First try

---
+++ src/probagit/ProbaGit.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package probagit;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author pavlo
+ */
+public class ProbaGit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        /*Задача 1
+        Пользователь вводит трехзначное число.
+        Найдите: Число единиц, десятков, cумму цифр.
+        Для решения можно использовать арифметический оператор % -остаток от деления.
+        */
+      
+        
+        System.out.println("Ведите трехзначное число: ");
+        Scanner scan = new Scanner(System.in);
+        int num = scan.nextInt();
+        
+        int units = num;
+        int tens = num / 10;
+        int hundreds = num /100;
+        
+        int summ = units + tens + hundreds;
+        
+        System.out.println("В заданном Вами числе единиц " + units);
+        System.out.println("В заданном Вами числе десятков " + tens);
+        System.out.println("В заданном Вами числе сотен " + hundreds);
+        System.out.println("Сумма всех цифр равна " + summ );
+        
+
+        
+    }
+    
+}
+
